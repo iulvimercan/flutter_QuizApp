0bf030275b4ce551f70bbc38901140045fbf0928
iulvimercan <ibrahimulvimercan@gmail.com>
2024-07-13 19:01:36 +0300

    visible scrollbar is added

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -15,57 +15,59 @@ class QuestionsSummary extends StatelessWidget {
       child: SizedBox(
         width: double.infinity,
         height: 450,
-        child: SingleChildScrollView(
-          child: Column(
-            children: questions.map((q) {
-              var questionNo = (questions.indexOf(q) + 1).toString();
+        child: Scrollbar(
+          child: SingleChildScrollView(
+            child: Column(
+              children: questions.map((q) {
+                var questionNo = (questions.indexOf(q) + 1).toString();
 
-              return Container(
-                margin: const EdgeInsets.only(bottom: 30),
-                child: SizedBox(
-                  child: Row(
-                    children: [
-                      QuestionNoContainer(questionNo, q.isCorrect()),
-                      const SizedBox(width: 18),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              q.question,
-                              style: GoogleFonts.lato(
-                                color: AppColors.whiteSoft,
-                                fontSize: 18,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                            Text(
-                              q.userAnswer ?? '~Empty~',
-                              style: TextStyle(
-                                color: q.isCorrect()
-                                    ? AppColors.correct
-                                    : AppColors.incorrect,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                            if(!q.isCorrect())
+                return Container(
+                  margin: const EdgeInsets.only(bottom: 30),
+                  child: SizedBox(
+                    child: Row(
+                      children: [
+                        QuestionNoContainer(questionNo, q.isCorrect()),
+                        const SizedBox(width: 18),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
                               Text(
-                                'Correct Answer: ${q.correctAnswer}',
-                                style: const TextStyle(
-                                  color: AppColors.warning,
-                                  fontSize: 15,
+                                q.question,
+                                style: GoogleFonts.lato(
+                                  color: AppColors.whiteSoft,
+                                  fontSize: 18,
                                   fontWeight: FontWeight.w600,
                                 ),
                               ),
-                          ],
+                              Text(
+                                q.userAnswer ?? '~Empty~',
+                                style: TextStyle(
+                                  color: q.isCorrect()
+                                      ? AppColors.correct
+                                      : AppColors.incorrect,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              if (!q.isCorrect())
+                                Text(
+                                  'Correct Answer: ${q.correctAnswer}',
+                                  style: const TextStyle(
+                                    color: AppColors.warning,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              );
-            }).toList(),
+                );
+              }).toList(),
+            ),
           ),
         ),
       ),
